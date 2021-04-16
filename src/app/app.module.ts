eacd7b514f621885e6b60341636b528201872ce4
MariaSilvaPeres <maria.silva.peres.com>
2021-04-16 20:12:18 +0100

    sfsd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { EventsListComponent } from './events/events-list.component';
+import { NavbarComponent } from './nav/navbar/navbar.component';
+import { EventService } from './events/shared/event.service';
+import { ToastrService } from './events/common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details/event-details.component';
+import { appRoutes } from './routes';
 
 @NgModule({
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   declarations: [
-    AppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavbarComponent,
+    EventDetailsComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [EventService, ToastrService],
+  bootstrap: [EventsAppComponent],
 })
-export class AppModule { }
+export class AppModule {}
