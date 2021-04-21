61cff8b9dfeea10a43e83420c9566d8e4106db74
MariaSilvaPeres <maria.silva.peres@hotmail.com>
2021-04-21 15:00:51 +0100

    asdasd

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -8,9 +8,9 @@ import { Router } from '@angular/router'
   styles: [`
     em { float: right; color: #E05C65; padding-left: 10px; }
     .error input { Background-color: #E3C3C5; }
-    
+
     /* There isn't a css standart for styling placeholders, so we have to do one for each browser */
-    
+
     . error ::-webkit-input-placeholder { color: #999; }
     . error ::-moz-placeholder { color: #999; }
     . error :-moz-placeholder { color: #999; }
@@ -21,49 +21,54 @@ import { Router } from '@angular/router'
 
 export class ProfileComponent implements OnInit {
 
-  profileForm:FormGroup
-  private firstName: FormControl; 
-  private lastName: FormControl;
+  profileForm:FormGroup;
+  firstName: FormControl;
+  lastName: FormControl;
 
   // get the data from authservice
   constructor(private router:Router, private authService:AuthService) {
+    //the form have two inputs, so we create a FormControl for each one.
+
+    this.firstName = new FormControl(this.authService.currentUser.firstName, [Validators.required, Validators.pattern('[a-zA-Z].*')])
+    this.lastName = new FormControl(this.authService.currentUser.lastName, [Validators.required, Validators.pattern('[a-zA-Z].*')])
+
+    // now we need to add these controls to a form. So we use a FormGroup for that
+    this.profileForm = new FormGroup({
+      firstName: this.firstName,
+      lastName: this.lastName
+
+    })
 
   }
 
-  ngOnInit() { 
-    //the form have two inputs, so we create a FormControl for each one.
-    this.firstName = new FormControl(this.authService.currentUser.firstName, [Validators.required, Validators.pattern('[a-zA-Z].*')])
-    this.lastName = new FormControl(this.authService.currentUser.lastName, [Validators.required, Validators.pattern('[a-zA-Z].*')])
-    
-    // now we need to add these controls to a form. So we use a FormGroup for that
-    this.profileForm = new FormGroup({ 
-      firstName: this.firstName,
-      lastName: this.lastName
-      
-    })
+  ngOnInit() {
+
+
+
+  }
+
+  validateFirstName() {
+    return this.firstName.valid || this.firstName.touched;
+  }
+
+  validateLastName() {
+    return this.lastName.valid || this.lastName.touched
   }
 
   cancel() {
     this.router.navigate(['events'])
   }
 
-  saveProfile(formValues: { firstName: string; lastName: string }) {
-    if (this.profileForm.valid) { 
+  saveProfile(formValues) {
+    if (this.profileForm.valid) {
     this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
     this.router.navigate(['events'])
-    } 
-  }
-
-  validateFirstName() {
-    return this.firstName.valid || this.firstName.touched
-  }
-
-  validateLastName() {
-    return this.lastName.valid ||
-    this.lastName.touched
+    }
   }
 
 
-  
-       
+
+
+
+
 }
